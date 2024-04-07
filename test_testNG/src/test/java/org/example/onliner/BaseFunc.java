2fc7b409a167e9b01943a94c8103db25ee2157b7
aqaacc <mralextb@gmail.com>
2024-04-07 20:28:12 +0200

    HW_9 WebDriver 2nd commit: updated baseFunc, 1st Test + 2nd Test

--- test_testNG/src/test/java/org/example/onliner/BaseFunc.java
+++ test_testNG/src/test/java/org/example/onliner/BaseFunc.java
@@ -1,31 +1,20 @@
 package org.example.onliner;
 
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseFunc {
     public final static String TV_CATEGORY_URL = "https://catalog.onliner.by/tv";
+
+    public static By xpathTvItems = By.xpath(".//div[@class='catalog-form__offers-part catalog-form__offers-part_data']//a[@class='catalog-form__link catalog-form__link_primary-additional catalog-form__link_base-additional catalog-form__link_font-weight_semibold catalog-form__link_nodecor']");
     public static WebDriver webDriver;
 
-    public void hardClick(WebElement webElement) {
-        JavascriptExecutor executor = (JavascriptExecutor) webDriver;
-        executor.executeScript("arguments[0].click();", webElement);
-    }
-
-    public void scrollToElement(WebElement webElement) {
-        JavascriptExecutor executor = (JavascriptExecutor) webDriver;
-        executor.executeScript("arguments[0].scrollIntoView(true);", webElement);
-        //executor.executeScript("window.scrollBy(0, 700);");
-        executor.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-    }
-
-    @BeforeMethod
+            @BeforeMethod
     public void setUp() {
         webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -33,10 +22,10 @@ public class BaseFunc {
         webDriver.get("https://www.onliner.by/");
     }
 
-    @AfterMethod
+    @AfterClass
     public void cleanup() {
-//        if (webDriver != null) {
-//            webDriver.quit();
-// }
+        if (webDriver != null) {
+            webDriver.quit();
+        }
     }
 }
--- test_testNG/src/test/java/org/example/onliner/OnlineTelevisionCategoryTest.java
+++ test_testNG/src/test/java/org/example/onliner/OnlineTelevisionCategoryTest.java
@@ -2,28 +2,62 @@ package org.example.onliner;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class OnlineTelevisionCategoryTest extends BaseFunc {
     public By COOKIE_LOCATOR =
             By.xpath(".//div[@class='fc-footer-buttons-container']//button//p[@class='fc-button-label'][contains(text(),'Соглашаюсь')]");
-    public static By TV_Category_LOCATOR = By.xpath(
-            ".//div[@class='catalog-form__checkbox-sign'][contains(text(), 'LG')][1]");
-    public static By LG_PAGE_TAG_LOCATOR = By.xpath(
-            ".//div[@class='button-style button-style_either button-style_small catalog-form__button catalog-form__button_tag']");
+    public By LG_PAGE_TAG_LOCATOR =
+            By.xpath(".//div[@class='button-style button-style_either button-style_small catalog-form__button catalog-form__button_tag']");
 
-    @Test
-    public void selectTelevisionCategory() {
+    @DataProvider(name = "tvCategory")
+    public Object[][] tvCategory() {
+        return new Object[][]{
+                {"LG"},
+                {"TCL"},
+                {"Hisense"},
+                {"Philips"},
+                {"Sony"},
+                {"Horizont"},
+                {"Blaupunkt"},
+                {"Яндекс"},
+                {"KIVI"}
+        };
+    }
+
+    private By getTvCategoryLocator(String tvCategory) {
+        return By.xpath(".//div[@class='catalog-form__checkbox-sign'][contains(text(), '" + tvCategory + "')]");
+    }
+
+    @Test(dataProvider = "tvCategory")
+    public void selectTelevisionCategory(String tvCategory) {
         //open TV category page
         webDriver.get(TV_CATEGORY_URL);
         JavascriptExecutor executor = (JavascriptExecutor) webDriver;
         executor.executeScript("arguments[0].click();", webDriver.findElement(COOKIE_LOCATOR));
 
-        //find TV category (LG)
-        scrollToElement(webDriver.findElement(TV_Category_LOCATOR));
+        //find TV category
+        By TV_Category_LOCATOR = getTvCategoryLocator(tvCategory);
         executor.executeScript("arguments[0].click();", webDriver.findElement(TV_Category_LOCATOR));
         Assert.assertEquals(webDriver.findElement(LG_PAGE_TAG_LOCATOR).getText(),
-                "LG", "Wrong page is opened");
+                tvCategory, "Wrong page is opened");
+    }
+
+    @Test(dataProvider = "tvCategory", dependsOnMethods = "selectTelevisionCategory")
+    public void validatingTvItemsName(String tvBrand) {
+        List<WebElement> tvElements = webDriver.findElements(xpathTvItems);
+        // Check if all TVs are manufactured by the specified brand
+        for (WebElement tvElement : tvElements) {
+            // Extract the manufacturer name for each TV
+            String manufacturer = tvElement.getText();
+            System.out.println(manufacturer);
+            // Assert that the manufacturer is the specified brand
+            Assert.assertTrue(manufacturer.toLowerCase().contains(tvBrand), "TV not manufactured by " + tvBrand + " found!");
+        }
     }
 }
