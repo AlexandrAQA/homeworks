f6f00d244f1cfca50584786debbba6d8f491abf8
aqaacc <mralextb@gmail.com>
2024-05-12 12:30:55 +0200

    sql_lite_hw_13: added 3 classes

---
+++ restAssured/hw13_sql/src/test/java/org/sqlLite/tables/Account.java
@@ -0,0 +1,53 @@
+package org.sqlLite.tables;
+
+public class Account {
+
+    private int accountId;
+    private int userId;
+    private double balance;
+    private String currency;
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Account(int accountId, int userId, double balance, String currency) {
+        this.accountId = accountId;
+        this.userId = userId;
+        this.balance = balance;
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return accountId + " | " + userId + " | " + balance + " | " + currency;
+    }
+}
---
+++ restAssured/hw13_sql/src/test/java/org/sqlLite/tables/User.java
@@ -0,0 +1,43 @@
+package org.sqlLite.tables;
+
+public class User {
+
+    private int userId;
+    private String name;
+    private String address;
+
+    public User(int userId, String name, String address) {
+        this.userId = userId;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return userId + " | " + name + " | " + address;
+    }
+}
